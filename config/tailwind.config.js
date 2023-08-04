3c64362a3be3d2270e301a69aadecf35f28f1e8c
Annie Sexton <anniesexton@Annies-M1.local>
2023-08-04 15:56:57 -0700

    styled homepage

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
@@ -12,6 +13,13 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        gray: colors.neutral,
+        tan: {
+          1: '#f8f4f2',
+          2: '#f0e8e5'
+        }
+      }
     },
   },
   plugins: [
